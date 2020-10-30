614e0a5886d981a65e33a503837aca39c09097a6
Freddy Drennan <drennanfreddy@gmail.com>
2020-10-30 04:42:59 -0600

    update

--- tests/testthat/test-client.R
+++ tests/testthat/test-client.R
@@ -1,3 +1,8 @@
+
+# AWS_ACCESS = system('echo $AWS_ACCESS')
+# AWS_SECRET = system('echo $AWS_SECRET')
+# AWS_REGION = system('echo $AWS_REGION')
+
 test_that("boto3 imports as expected", {
   testthat::expect_equal(
     class(biggr2::boto3()),
